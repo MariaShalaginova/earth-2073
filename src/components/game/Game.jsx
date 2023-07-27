e3140e96bda97ca899d71a28e221976771eac5c9
Maria Shalaginova <pro_m_@mail.ru>
2023-07-27 15:16:09 +0300

    добавлена плавность смены сцен

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -10,13 +10,13 @@ import classNames from 'classnames';
 const Games = () => {
   const [dialogues, setDialogues] = useState([]); // в этот массив придет dialogues
   const navigate = useNavigate();
-  const typingSpeed = 30; //Скорость печати в миллисекундах 
+  const typingSpeed = 25; //Скорость печати в миллисекундах 
   const [currentScene, setCurrentScene] = useState(0);
   // const [isAddStyle, setIsAddStyle] = useState(true);
   // const [currentWindowIndex, setCurrentWindowIndex] = useState(0); //для сохранения в локал текущего диалога
   const [currentDialog, setCurrentDialog] = useState(0);  
   const [nextDialog, setNextDialog] = useState(false);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
+  // const [imagesLoaded, setImagesLoaded] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -55,66 +55,6 @@ const Games = () => {
       console.error('Ошибка:', error);
     }
   };
-
-
-  // useEffect(() => {
-  //   const image = new Image();
-  //   image.src = src;
-  //   image.onload = () => {
-  //     setLoading(false);
-  //   };
-  // }, [src]);
-
-  // useEffect(() => {
-  //   // Проверяем, все ли изображения загружены
-  //   if (dialogues) {
-  //     const images = [];
-  //     dialogues.forEach(item => {
-  //       const img = new Image();
-  //       img.src = item.imagePath;
-  //       images.push(img);
-  //     });
-
-  //     Promise.all(images.map(img => {
-  //       return new Promise((resolve, reject) => {
-  //         img.onload = resolve;
-  //         img.onerror = reject;
-  //       });
-  //     }))
-  //       .then(() => {
-  //         setImagesLoaded(true); // Все изображения загружены
-  //       })
-  //       .catch(error => {
-  //         console.error(error);
-  //       });
-  //   }
-  // }, [data]); 
-
-//   useEffect(() => {
-//     const images = [
-//       require('../../\'+dialogues[currentScene].scene[0].path_img)'),
-//      require('../../\'+dialogues[currentScene].windows[currentDialog].path_img)')
-//     ];
-// console.log(images);
-//     const loadImage = (image) => {
-//       return new Promise((resolve) => {
-//         const img = new Image();
-//         img.src = image;
-//         img.onload = () => resolve();
-//       });
-//     };
-
-//     Promise.all(images.map((image) => loadImage(image))).then(() => {
-//       setImagesLoaded(true);
-//     });
-//   }, []);
-
-  // const backgroundImageStyle = {
-  //   backgroundImage: `url(${require('../../'+dialogues[currentScene].scene[0].path_img)})`,
-  //   backgroundSize: "cover",
-  //   backgroundPosition: "center",
-  //   transition: "background-image 0.5s ease-in-out",
-  // };
 
   const handlePrevDialog = () => {
     let dialogLength = dialogues[currentScene].windows.length;
@@ -172,29 +112,30 @@ const Games = () => {
               
               <div className={classNames(css.scene, css.sceneFade)}>
               {/* <img className={css.sceneImg} src={dialogues[currentScene].scene[0].path_img}  alt={dialogues[currentScene].scene[0].name}/> */}
-              <img className={css.sceneImg} src={require('../../'+dialogues[currentScene].scene[0].path_img)} alt={dialogues[currentScene].scene[0].name}/>
+              <img className= {classNames(
+                (
+                  dialogues[currentScene].windows[currentDialog].window_id === 0 && nextDialog && dialogues[currentScene].windows[currentDialog].text !== "0"
+                ) || (
+                  dialogues[currentScene].windows[currentDialog].window_id === 1 && nextDialog && dialogues[currentScene].windows[currentDialog-1].text === "0"
+                )  ? css.sceneFade : '' )}
+                 src={require('../../'+dialogues[currentScene].scene[0].path_img)} alt={dialogues[currentScene].scene[0].name}/>
               <div className={css.backMain}>
                 <Button onClick={async event => {navigate('/start')}}>Главная страница</Button>
               </div>
-              {/* { dialogues[currentScene].windows[currentDialog].position === 'left' ? (
-                <div className={css.positionLeft}>
-                  <img className={css.positionChar} src={require('../../'+dialogues[currentScene].windows[currentDialog].path_img)} alt={dialogues[currentScene].windows[currentDialog].character}/>
-                </div> 
-              ) : (
-                <div className={css.positionRight}>
-                  <img className={css.positionChar} src={require('../../'+dialogues[currentScene].windows[currentDialog].path_img)} alt={dialogues[currentScene].windows[currentDialog].character}/>
-                </div> 
-              )} */}
-              {/* <div className={classNames(css.position,  dialogues[currentScene].windows[currentDialog].position === 'left' ? css.positionLeft : css.positionRight, dialogues[currentScene].windows[currentDialog].position === '0' ? css.windowHide : '')} > */}
 
-              <div className={classNames(css.position,  dialogues[currentScene].windows[currentDialog].position === '0' ? css.windowHide : '')} >
+              <div className={classNames(css.position,  dialogues[currentScene].windows[currentDialog].position === '0' ? css.windowHide : '', (
+                  dialogues[currentScene].windows[currentDialog].window_id === 0 && nextDialog && dialogues[currentScene].windows[currentDialog].text !== "0"
+                ) || (
+                  dialogues[currentScene].windows[currentDialog].window_id === 1 && nextDialog && dialogues[currentScene].windows[currentDialog-1].text === "0"
+                )  ? css.sceneFade : '')} >
                   <img className={classNames(css.positionChar,  dialogues[currentScene].windows[currentDialog].position === 'left' ? css.positionLeft : css.positionRight)} src={require('../../'+dialogues[currentScene].windows[currentDialog].path_img)} alt={dialogues[currentScene].windows[currentDialog].character}/>
                 </div> 
 
-              {/* <div className={css.positionRight}>
-                <img className={css.positionChar} src={require('../../'+dialogues[currentScene].windows[currentDialog].path_img)} alt={dialogues[currentScene].windows[currentDialog].character}/>
-              </div>   */}
-              <div className={classNames(css.window, dialogues[currentScene].windows[currentDialog].text === '0' ? css.windowHide : '')}>
+              <div className={classNames(css.window, dialogues[currentScene].windows[currentDialog].text === '0' ? css.windowHide : '',(
+                  dialogues[currentScene].windows[currentDialog].window_id === 0 && nextDialog && dialogues[currentScene].windows[currentDialog].text !== "0"
+                ) || (
+                  dialogues[currentScene].windows[currentDialog].window_id === 1 && nextDialog && dialogues[currentScene].windows[currentDialog-1].text === "0"
+                )  ? css.sceneFade : '')}>
                 <div className={css.character}>{dialogues[currentScene].windows[currentDialog].character}</div> 
                 <div className={css.message}>
                   <TypewriterText
--- src/components/game/ImageLoad.jsx
+++ src/components/game/ImageLoad.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const ImageComponent = ({ src }) => {
-    const [loading, setLoading] = useState(true);
+// const ImageComponent = ({ src }) => {
+//     const [loading, setLoading] = useState(true);
   
-    useEffect(() => {
-      const image = new Image();
-      image.src = src;
-      image.onload = () => {
-        setLoading(false);
-      };
-    }, [src]);
+//     useEffect(() => {
+//       const image = new Image();
+//       image.src = src;
+//       image.onload = () => {
+//         setLoading(false);
+//       };
+//     }, [src]);
   
-    return loading ? <p>Loading...</p> : <img src={src} alt="scene image" />;
-  };
+//     return loading ? <p>Loading...</p> : <img src={src} alt="scene image" />;
+//   };
   
-  export default ImageComponent;
+//   export default ImageComponent;
